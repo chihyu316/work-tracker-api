779c29483f8bbed720e0f15b92e3a3e4e1525962
chihyu316 <bright.yu.yu@gmail.com>
2026-04-14 16:01:24 +0800

    Add files via upload

--- api/list.js
+++ api/list.js
@@ -8,13 +8,8 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-
-    if (req.method !== 'GET') {
-        return res.status(405).json({ error: '只接受 GET 請求' });
-    }
+    if (req.method === 'OPTIONS') return res.status(200).end();
+    if (req.method !== 'GET') return res.status(405).json({ error: '只接受 GET 請求' });
 
     try {
         const response = await notion.databases.query({
@@ -24,11 +19,11 @@ module.exports = async (req, res) => {
 
         const records = response.results.map(page => ({
             id: page.id,
-            title: page.properties.項目名稱.title[0]?.plain_text || '',
-            date: page.properties.日期.date?.start || '',
-            type: page.properties.類型.select?.name || '',
-            rating: page.properties.評分.number || 0,
-            note: page.properties.備註.rich_text[0]?.plain_text || ''
+            title: page.properties['項目名稱'].title[0]?.plain_text || '',
+            date: page.properties['日期'].date?.start || '',
+            type: page.properties['類型'].select?.name || '',
+            rating: page.properties['評分'].number || 0,
+            note: page.properties['備註'].rich_text[0]?.plain_text || ''
         }));
 
         return res.status(200).json({ success: true, records });
